d8872aea4dab0d438451e735888327c1228ec06e
Andrés Estrella <andresmauricioestrella@gmail.com>
2022-10-05 19:26:58 -0400

    app permisos funcionando

--- tarea2-permisos/app/src/main/java/com/example/primero/DisplayMessageActivity.java
+++ tarea2-permisos/app/src/main/java/com/example/primero/DisplayMessageActivity.java
@@ -1,24 +1,142 @@
 package com.example.primero;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class DisplayMessageActivity extends AppCompatActivity {
+    private TextView storageText, locationText, cameraText, phoneText, contactText;
+    private String cameraPackageName = "com.android.camera";
+    private String contactsPackageName = "com.android.contacts";
+    private String filesPackageName = "com.android.files";
+    private String TELEFONO = "809-581-1111";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        storageText = findViewById(R.id.storageText);
+        storageText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    Context ctx = DisplayMessageActivity.this;
+                    Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                    Intent intent=new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS);
+                    try {
+                        startActivity(intent);
+                    }catch (ActivityNotFoundException e){
+                        Toast.makeText(ctx,"No hay aplicacion disponible para esta tarea",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast t = Toast.makeText(DisplayMessageActivity.this, "Please request permission", Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.FILL_HORIZONTAL,0,0);
+                    t.show();
+                }
+            }
+        });
+        locationText = findViewById(R.id.locationText);
+        locationText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    Context ctx = DisplayMessageActivity.this;
+                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                            Uri.parse("geo:0,0?q=santiago+republica+dominicana"));
+                    try {
+                        ctx.startActivity(intent);
+                    }catch (ActivityNotFoundException e){
+                        Toast.makeText(ctx,"No hay aplicacion disponible para esta tarea",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast t = Toast.makeText(DisplayMessageActivity.this, "Please request permission", Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.FILL_HORIZONTAL,0,0);
+                    t.show();
+                }
+            }
+        });
 
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+        cameraText = findViewById(R.id.cameraText);
+        cameraText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                    Context ctx = DisplayMessageActivity.this;
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    try {
+                        ctx.startActivity(takePictureIntent);
+                    }catch (ActivityNotFoundException e){
+                        Toast.makeText(ctx,"No hay aplicacion disponible para esta tarea",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast t = Toast.makeText(DisplayMessageActivity.this, "Please request permission", Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.FILL_HORIZONTAL,0,0);
+                    t.show();
+                }
+            }
+        });
+
+        phoneText = findViewById(R.id.phoneText);
+        phoneText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+                    Context ctx = DisplayMessageActivity.this;
+                    String uri = "tel:" + TELEFONO.trim();
+                    Intent i = new Intent(Intent.ACTION_CALL);
+                    //Intent i = new Intent(Intent.ACTION_DIAL);
+                    i.setData(Uri.parse(uri));
+                    try {
+                        startActivity(i);
+                    }catch (ActivityNotFoundException e){
+                        Toast.makeText(ctx,"No hay aplicacion disponible para esta tarea",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast t = Toast.makeText(DisplayMessageActivity.this, "Please request permission", Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.FILL_HORIZONTAL,0,0);
+                    t.show();
+                }
+            }
+        });
+
+        contactText = findViewById(R.id.contactText);
+        contactText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
+                    Context ctx = DisplayMessageActivity.this;
+                    Intent i = new Intent();
+                    i.setComponent(new ComponentName(contactsPackageName, "com.android.contacts.DialtactsContactsEntryActivity"));
+                    i.setAction("android.intent.action.MAIN");
+                    i.addCategory("android.intent.category.LAUNCHER");
+                    i.addCategory("android.intent.category.DEFAULT");
+                    try {
+                        startActivity(i);
+                    }catch (ActivityNotFoundException e){
+                        Toast.makeText(ctx,"No hay aplicacion disponible para esta tarea",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast t = Toast.makeText(DisplayMessageActivity.this, "Please request permission", Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.FILL_HORIZONTAL,0,0);
+                    t.show();
+                }
+            }
+        });
     }
+
 
 }
--- tarea2-permisos/app/src/main/java/com/example/primero/MainActivity.java
+++ tarea2-permisos/app/src/main/java/com/example/primero/MainActivity.java
@@ -1,31 +1,187 @@
 package com.example.primero;
 
-import android.widget.TextView;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.*;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.primero.MESSAGE";
+
+    private Button cancelButton;
+    private Switch storageSwitch, locationSwitch, cameraSwitch, phoneSwitch, contactSwitch;
+
+    private static final int STORAGE_PERMISSION_CODE = 100;
+    private static final int LOCATION_PERMISSION_CODE = 101;
+    private static final int CAMERA_PERMISSION_CODE = 102;
+    private static final int PHONE_PERMISSION_CODE = 103;
+    private static final int CONTACT_PERMISSION_CODE = 104;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        checkPermissions();
+        setOnClickListeners();
+
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                storageSwitch.setChecked(true);
+            }else{
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (requestCode == LOCATION_PERMISSION_CODE){
+            if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                locationSwitch.setChecked(true);
+            }else{
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (requestCode == CAMERA_PERMISSION_CODE){
+            if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                cameraSwitch.setChecked(true);
+            }else{
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (requestCode == PHONE_PERMISSION_CODE){
+            if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                phoneSwitch.setChecked(true);
+            }else{
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (requestCode == CONTACT_PERMISSION_CODE){
+            if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                contactSwitch.setChecked(true);
+            }else{
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void setOnClickListeners() {
+
+        storageSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(storageSwitch.isChecked()){
+                    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                }else{
+                    storageSwitch.setChecked(true);
+                    Toast.makeText(MainActivity.this, "Permiso ya fue otorgado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        locationSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(locationSwitch.isChecked()){
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
+                }else{
+                    locationSwitch.setChecked(true);
+                    Toast.makeText(MainActivity.this, "Permiso ya fue otorgado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        cameraSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(cameraSwitch.isChecked()){
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+                }else{
+                    cameraSwitch.setChecked(true);
+                    Toast.makeText(MainActivity.this, "Permiso ya fue otorgado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        phoneSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(phoneSwitch.isChecked()){
+                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_PERMISSION_CODE);
+                }else{
+                    phoneSwitch.setChecked(true);
+                    Toast.makeText(MainActivity.this, "Permiso ya fue otorgado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        contactSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(contactSwitch.isChecked()){
+                    requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, CONTACT_PERMISSION_CODE);
+                }else{
+                    contactSwitch.setChecked(true);
+                    Toast.makeText(MainActivity.this, "Permiso ya fue otorgado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
     //Called when the user taps the Send button
-    public void sendMessage(View view) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public void clickNext(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //intent.setAction(Intent.ACTION_SEND);
+        //intent.setType("text/plain");
         startActivity(intent);
+    }
+
+    private void  checkPermissions(){
+        storageSwitch = findViewById(R.id.storageSwitch);
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            storageSwitch.setChecked(true);
+        }else
+            storageSwitch.setChecked(false);
+
+        locationSwitch = findViewById(R.id.locationSwitch);
+        if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationSwitch.setChecked(true);
+        }else
+            locationSwitch.setChecked(false);
+
+        cameraSwitch = findViewById(R.id.cameraSwitch);
+        if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            cameraSwitch.setChecked(true);
+        }else
+            cameraSwitch.setChecked(false);
+
+        phoneSwitch = findViewById(R.id.phoneSwitch);
+        if(checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+            phoneSwitch.setChecked(true);
+        }else
+            phoneSwitch.setChecked(false);
+
+        contactSwitch = findViewById(R.id.contactSwitch);
+        if(checkSelfPermission(Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
+            contactSwitch.setChecked(true);
+        }else
+            contactSwitch.setChecked(false);
     }
 
 }
